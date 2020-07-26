06e9c960539843ef6af5aee142e48f82fe205ab2
Marcel maro <Marcel_maro@outlook.com>
2020-07-25 23:25:47 -0700

    Updates

---
+++ precache-manifest.d335255f33fdfa48c7f521dd05c180f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7478f5a3072fb2ad2246ee789e09d6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6424371037dda57e6227",
+    "url": "/static/css/main.5370cc5b.chunk.css"
+  },
+  {
+    "revision": "e0ed94bbd52870c9c783",
+    "url": "/static/js/2.019902b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.019902b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6424371037dda57e6227",
+    "url": "/static/js/main.e51c508a.chunk.js"
+  },
+  {
+    "revision": "880ead2686ec4d3085c1",
+    "url": "/static/js/runtime-main.b40e28e2.js"
+  }
+]);
